cc4ed30e9aae06799b915828a0410cadc0f5111e
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2024-03-12 17:13:15 +0330

    Update nptrial.py

--- nptrial.py
+++ nptrial.py
@@ -1,116 +1,72 @@
 import secrets
 import string
 import requests
+import logging
+
+# Setup basic logging
+logging.basicConfig(level=logging.INFO)
 
 # Constants
 PASSWORD_LENGTH = 12
 PHONE_FORMAT = '{:3}-{:3}-{:4}'
-DOMAINS_URL = 'https://api.mail.tm/domains'
-ACCOUNTS_URL = 'https://api.mail.tm/accounts'
-TOKEN_URL = 'https://api.mail.tm/token'
-MESSAGES_URL = 'https://api.mail.tm/messages'
+API_BASE_URL = 'https://api.mail.tm'
+DOMAINS_URL = f'{API_BASE_URL}/domains'
+ACCOUNTS_URL = f'{API_BASE_URL}/accounts'
+TOKEN_URL = f'{API_BASE_URL}/token'
+MESSAGES_URL = f'{API_BASE_URL}/messages'
 NESSUS_TRIAL_URL = "https://www.tenable.com/evaluations/api/v2/trials"
 HEADERS = {"Content-Type": "application/json"}
 
-# Function to handle HTTP GET requests
-def make_get_request(url, headers={}):
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-    return response.json()
+def perform_http_request(method, url, headers=None, json_data=None):
+    headers = headers or HEADERS
+    try:
+        response = requests.request(method, url, headers=headers, json=json_data)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        logging.error(f"Request error for {url}: {e}")
+        raise
 
-# Function to handle HTTP POST requests
-def make_post_request(url, headers={}, json_data={}):
-    response = requests.post(url, headers=headers, json=json_data)
-    response.raise_for_status()
-    return response.json()
+def gen_random_string(length, chars=string.ascii_lowercase):
+    return ''.join(secrets.choice(chars) for _ in range(length))
 
-# Generate Random 8-Char String
-def gen_name():
-    return ''.join(secrets.choice(string.ascii_lowercase) for _ in range(8))
-
-# Generate Random Password
 def gen_pass():
     characters = string.ascii_letters + string.digits + "!@#$%^&*()"
-    return ''.join(secrets.choice(characters) for _ in range(PASSWORD_LENGTH))
+    return gen_random_string(PASSWORD_LENGTH, characters)
 
-# Registers Random Mail on mail.tm
-def mail_generator(random_name, random_password):
-    domain_data = make_get_request(DOMAINS_URL)
-    domain = domain_data["hydra:member"][0]["domain"]
-    body = {"address": f"{random_name}@{domain}", "password": random_password}
-    account_data = make_post_request(ACCOUNTS_URL, headers=HEADERS, json_data=body)
-    return account_data["address"]
-
-# Get Authorization Token from mail provider
-def get_token(email, password):
-    body = {"address": email, "password": password}
-    token_data = make_post_request(TOKEN_URL, headers=HEADERS, json_data=body)
-    return token_data["id"], token_data["token"]
-
-# Get mail Count
-def get_mail_count(token):
-    headers = {"Authorization": f"Bearer {token}"}
-    message_data = make_get_request(MESSAGES_URL, headers=headers)
-    return message_data["hydra:totalItems"]
-
-# Get New Mail ID
-def get_mail_id(token):
-    headers = {"Authorization": f"Bearer {token}"}
-    message_data = make_get_request(MESSAGES_URL, headers=headers)
-    return message_data["hydra:member"][0]["id"]
-
-# Get Mail Referred to Providing ID
-def get_mail(mid, token):
-    headers = {"Authorization": f"Bearer {token}"}
-    message_data = make_get_request(f"{MESSAGES_URL}/{mid}", headers=headers)
-    return message_data["html"][0]
-
-# Generate Random Phone Number
 def gen_phone():
-    first = secrets.randbelow(900) + 100
-    second = secrets.randbelow(888) + 1
-    last = secrets.randbelow(9998) + 1
-    while last in {1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888}:
-        last = secrets.randbelow(9998) + 1
+    parts = (
+        secrets.randbelow(900) + 100,
+        secrets.randbelow(900) + 100,
+        secrets.randbelow(9000) + 1000,
+    )
+    return PHONE_FORMAT.format(*parts)
 
-    return PHONE_FORMAT.format(first, second, last)
-
-# Request Nessus Pro Trial
-def request_trial(random_name, random_email, random_num):
-    body = {
-        "skipContactLookup": "true",
-        "product": "expert",
-        "first_name": random_name,
-        "last_name": random_name,
-        "email": random_email,
-        "phone": random_num,
-        "title": random_name,
-        "company": random_name,
-        "companySize": "1-9",
-        "apps": ["expert"]
-    }
-    response = make_post_request(NESSUS_TRIAL_URL, headers=HEADERS, json_data=body)
-    return response["trial"]
+def register_email_account(name, password):
+    domain = perform_http_request('GET', DOMAINS_URL)['hydra:member'][0]['domain']
+    account_data = perform_http_request('POST', ACCOUNTS_URL, json_data={"address": f"{name}@{domain}", "password": password})
+    return account_data["address"]
 
 def main():
     try:
-        # Generate Random Details
-        random_name = gen_name()
+        random_name = gen_random_string(8)
         random_password = gen_pass()
-        random_number = gen_phone()
-        email = mail_generator(random_name, random_password)
-        print(f"Your TempMail is: {email}")
-        print(f"Your Password is: {random_password}")
+        random_phone = gen_phone()
+        email = register_email_account(random_name, random_password)
+        logging.info(f"Your TempMail is: {email}\nYour Password is: {random_password}")
 
-        # Request Trial
-        nessus_trial_response = request_trial(random_name, email, random_number)
-        print(f"Nessus Trial Response: {nessus_trial_response}")
-        print(f"Use nessuscli.exe fetch --register {nessus_trial_response['code']} to activate your product")
-        print("You can also use https://plugins.nessus.org/v2/offline.php for offline activation")
-    except requests.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")  # HTTP error
-    except Exception as err:
-        print(f"An error occurred: {err}")  # Other errors
+        trial_info = perform_http_request('POST', NESSUS_TRIAL_URL, json_data={
+            "first_name": random_name, "last_name": random_name,
+            "email": email, "phone": random_phone, "company": random_name,
+            "companySize": "1-9", "product": "expert", "apps": ["expert"], "skipContactLookup": "true"
+        })
+        logging.info(f"Nessus Trial Response: {trial_info}")
+        logging.info(f"Use nessuscli.exe fetch --register {trial_info['trial']['code']} to activate your product")
+        logging.info("You can also use https://plugins.nessus.org/v2/offline.php for offline activation")
+    except requests.HTTPError as e:
+        logging.error(f"HTTP error occurred: {e}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     main()
